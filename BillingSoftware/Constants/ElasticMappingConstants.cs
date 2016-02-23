a2889c566ce92695d59733b5beeb2bdd61e0947a
vignesh <vignesh.jag@gmail.com>
2016-02-23 22:29:51 +0530

    added product model

--- BillingSoftware/Constants/ElasticMappingConstants.cs
+++ BillingSoftware/Constants/ElasticMappingConstants.cs
@@ -9,6 +9,7 @@ namespace BillingSoftware.Constants
     {
         public const string INDEX_NAME = "billing";
         public const string TYPE_ADMIN = "admin";
+        public const string TYPE_PRODUCT = "product";
         public const string ID = "id";
         public const string ELASTIC_SEARCH_URL = "http://127.0.0.1:9200/";
     }
@@ -20,6 +21,16 @@ namespace BillingSoftware.Constants
         public const string PASSWORD = "password";
         public const string SALT = "salt";
         public const string TYPE = "type";
+        public const string CREATED_AT = "created_at";
+    }
+
+    public class ConstProduct
+    {
+        public const string ID = "id";
+        public const string PRODUCT_NAME = "product_name";
+        public const string PRICE = "price";
+        public const string QUANTITY = "quantity";
+        public const string UNIT = "unit";
         public const string CREATED_AT = "created_at";
     }
 }
---
+++ BillingSoftware/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using BillingSoftware.Constants;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillingSoftware.Models
+{
+    [ElasticType(IdProperty = ElasticMappingConstants.ID, Name = ElasticMappingConstants.INDEX_NAME)]
+    public class Product
+    {
+        [ElasticProperty(Name =ConstProduct.ID, Index =FieldIndexOption.NotAnalyzed)]
+        public string id { get; set; }
+
+        [ElasticProperty(Name =ConstProduct.PRODUCT_NAME, Index =FieldIndexOption.Analyzed)]
+        public string productname { get; set; }
+
+        [ElasticProperty(Name =ConstProduct.PRICE, Index =FieldIndexOption.NotAnalyzed)]
+        public double price { get; set; }
+
+        [ElasticProperty(Name =ConstProduct.QUANTITY, Index =FieldIndexOption.NotAnalyzed)]
+        public float quantity { get; set; }
+
+        [ElasticProperty(Name =ConstProduct.UNIT, Index =FieldIndexOption.NotAnalyzed)]
+        public Int16 unit { get; set; }
+
+        [ElasticProperty(Name =ConstProduct.CREATED_AT, Index =FieldIndexOption.NotAnalyzed)]
+        public DateTime create_at { get; set; }
+    }
+}
