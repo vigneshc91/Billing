cdece76c49fb11ac375e56f0921625b2a5688e9e
vignesh <vignesh.jag@gmail.com>
2016-03-02 22:46:15 +0530

    added elastic mapping constants for other types

--- BillingSoftware/Constants/ElasticMappingConstants.cs
+++ BillingSoftware/Constants/ElasticMappingConstants.cs
@@ -10,6 +10,9 @@ namespace BillingSoftware.Constants
         public const string INDEX_NAME = "billing";
         public const string TYPE_ADMIN = "admin";
         public const string TYPE_PRODUCT = "product";
+        public const string TYPE_USER = "user";
+        public const string TYPE_SALES = "sales";
+        public const string TYPE_SALES_INFO = "sales_info";
         public const string ID = "id";
         public const string ELASTIC_SEARCH_URL = "http://127.0.0.1:9200/";
     }
@@ -32,5 +35,41 @@ namespace BillingSoftware.Constants
         public const string QUANTITY = "quantity";
         public const string UNIT = "unit";
         public const string CREATED_AT = "created_at";
+    }
+
+    public class ConstUser
+    {
+        public const string ID = "user_id";
+        public const string NAME = "name";
+        public const string ADDR_1 = "address_1";
+        public const string ADDR_2 = "address_2";
+        public const string CITY = "city";
+        public const string DISTRICT = "district";
+        public const string STATE = "state";
+        public const string COUNTRY = "country";
+        public const string PIN_CODE = "pin_code";
+        public const string PHONE = "phone";
+        public const string EMAIL = "email";
+        public const string CREATED_AT = "created_at";
+    }
+
+    public class ConstSales
+    {
+        public const string ID = "sales_id";
+        public const string DATE_TIME = "date_time";
+        public const string USER_ID = "user_id";
+        public const string NAME = "name";
+        public const string TAX = "tax";
+        public const string DISCOUNT = "discount";
+        public const string STATUS = "status";
+        public const string CREATED_AT = "created_at";
+    }
+
+    public class ConstSalesInfo
+    {
+        public const string SALES_ID = "sales_id";
+        public const string PRODUCT_ID = "product_id";
+        public const string QUANTITY = "quantity";
+        public const string PRICE = "price";
     }
 }
